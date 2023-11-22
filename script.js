ec766a99b91a0d56ce358f2b80e8da1d23e64abe
tobias10kodehode <tobias10.kodehode@gmail.com>
2023-11-22 12:49:58 +0100

    *

---
+++ script.js
@@ -0,0 +1,16 @@
+const nameGreet = document.getElementById("name-inputfield")
+const birth = document.getElementById("age-inputfield")
+const nameColor = document.getElementById("name-color-inputfield")
+
+
+function greet() {
+
+}
+
+function birthYear() {
+
+}
+
+function nameColorChange() {
+
+}
